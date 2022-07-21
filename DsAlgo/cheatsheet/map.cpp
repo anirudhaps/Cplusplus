e1da05144509659a5b6a6bb127d75374750851f8
Anirudha Singh <ps.anirudha@gmail.com>
2022-07-21 22:37:10 +0530

    add more info about the behavior of maps

--- DsAlgo/cheatsheet/map.cpp
+++ DsAlgo/cheatsheet/map.cpp
@@ -2,8 +2,18 @@
 #include <unordered_map>
 
 int main() {
-    std::unordered_map<char, char> my_map;
-    if (my_map['j'] == '\0')
-        std::cout << "yes" << std::endl;
-    return 0;
+  std::unordered_map<char, char> my_map;
+  std::cout << my_map.size() << "\n";
+  if (my_map['j'] == '\0')
+    std::cout << "yes" << std::endl;
+  // When we check for existence of a key in a map (as it is done above) and it
+  // is absent in the map, it is added to the map and it increases the size of
+  // the map by 1. It is added with zero value of the value_type. That is, even
+  // if we compare my_map['j'] == 'a', this will add ['j' : '\0'] entry in the
+  // map. These facts are true for both std::map and std::unordered_map.
+  std::cout << my_map.size() << "\n";
+  for (auto p : my_map) {
+    std::cout << p.first << ": " << p.second << "\n";
+  }
+  return 0;
 }
